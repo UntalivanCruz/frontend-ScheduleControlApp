456168b7627abca08958e1700e2002719480c43b
Ivan Cruz <ivancruzhn@gmail.com>
2023-01-05 01:50:27 -0600

    template attendance && Employee

--- src/pages/Attendance.page.tsx
+++ src/pages/Attendance.page.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { Button, Form, Input, message, Space } from 'antd';
 
 export const Attendance = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = () => {
+    message.success('Submit success!');
+  };
+
+  const onFinishFailed = () => {
+    message.error('Submit failed!');
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      identification: '123456',
+    });
+  };
   return (
+    <>
     <div>Welcome!</div>
+    <Form
+      form={form}
+      layout="vertical"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        name="identification"
+        label="Identification"
+        rules={[{ required: true }, { type: 'string', min: 6 }]}
+      >
+        <Input placeholder="input your identification number" />
+      </Form.Item>
+      <Form.Item>
+        <Space>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Button htmlType="button" onClick={onFill}>
+            Fill
+          </Button>
+        </Space>
+      </Form.Item>
+    </Form>
+    </>
   )
 }
--- src/pages/Employee.page.tsx
+++ src/pages/Employee.page.tsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import {Table, Switch, Space} from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import type { ColumnsType } from 'antd/es/table';
+import { ApiError,EmployeeWithRelations, EmployeeControllerService } from '../services/index'
+
+const columns: ColumnsType<EmployeeWithRelations> = [
+  {
+    title: 'Identification',
+    dataIndex: 'identification',
+    key: 'identification',
+  },
+  {
+    title: 'First Name',
+    dataIndex: 'firstName',
+    key: 'firstName',
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'lastName',
+    key: 'lastName',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: 'gender',
+  },
+  {
+    title: 'Nationality',
+    dataIndex: 'nationality',
+    key: 'nationality',
+  },
+  {
+    title: 'Positions',
+    dataIndex: 'EmployeePositions',
+    key: 'EmployeePositions',
+    render: (_, {EmployeePositions} ) => (
+      <>{ EmployeePositions?.name }</>
+    ),
+  },
+  {
+    title: 'State',
+    dataIndex: 'state',
+    key: 'state',
+    render: (_, { state }) => (
+      <Switch checkedChildren="Enabled" unCheckedChildren="Disabled" defaultChecked checked={state} />
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <EditOutlined />
+        <DeleteOutlined />
+      </Space>
+    )
+  },
+];
 
 export const Employee = () => {
+  const [data, setData] = useState<EmployeeWithRelations[]>([]);
+  const [error, setError] = useState<ApiError|null>();
+
+useEffect(() => {
+  EmployeeControllerService.employeeControllerFind(
+    '{"include":[{"relation":"EmployeePositions"}]}'
+  )
+    .then((originData) => setData(originData))
+    .catch((error) => setError(error));
+}, []);
   return (
-    <div>Employee</div>
+    <>
+    <h1>Employee</h1>
+    <Table columns={columns} dataSource={data} rowKey="id"/>
+    {error}
+    </>
   )
 }
