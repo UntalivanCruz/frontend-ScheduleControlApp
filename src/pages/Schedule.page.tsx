5f3a3ce8b3bd3ae3c7a73e8471cc09f2d4c357c4
Ivan Cruz <ivancruzhn@gmail.com>
2023-01-05 01:52:36 -0600

    bug fix Schedule model not used

--- src/pages/Schedule.page.tsx
+++ src/pages/Schedule.page.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import {Table, Switch, Space} from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import type { ColumnsType } from 'antd/es/table';
-import { ApiError,ScheduleWithRelations, ScheduleControllerService, Schedule_IncludeFilter_Items} from '../services/index'
+import { ApiError,ScheduleWithRelations, ScheduleControllerService} from '../services/index'
 
 const columns: ColumnsType<ScheduleWithRelations> = [
   {
